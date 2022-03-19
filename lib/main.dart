d865be62ac29455791d19c65b27af0ddb9bfb442
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-19 19:39:59 +0300

    changed the build in method

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          // Add the const
-          //child: Text(wordPair.asPascalCase), // REPLACE with...
-          child: RandomWords(), // ...this line
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
