da60a374bb01cca31cd1e460c62af3ba0d771578
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-20 23:59:14 +0300

    favorite Icons

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[]; // NEW
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18); // NEW
   @override
   @override
@@ -77,10 +78,16 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair); //NEW
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'save',
       ),
     );
   }
